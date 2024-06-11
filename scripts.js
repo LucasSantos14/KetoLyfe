68af579b217ccd43a18565b41adb423e9b4de4eb
Lucas Santos <115504039+LucasSantos14@users.noreply.github.com>
2024-06-11 15:19:25 -0300

    Teste 2

--- scripts.js
+++ scripts.js
@@ -1,33 +1,32 @@
+// document.addEventListener('contextmenu', function (e) {
+//     e.preventDefault()
+// })
+
 function buyNow() {
-    window.location.href = "https://pay.kiwify.com.br/OXZo2kX";
+    window.location.href = "https://pay.kiwify.com.br/OXZo2kX"
 }
 
 function togglePlayPause() {
-    const video = document.getElementById('vsl-video');
-    const playPauseButton = document.getElementById('play-pause-button');
+    const video = document.getElementById('vsl-video')
+    const playPauseButton = document.getElementById('play-pause-button')
 
     if (video.paused) {
-        video.play();
-        playPauseButton.textContent = "Pausar";
+        video.play()
+        playPauseButton.textContent = "Pausar"
     } else {
-        video.pause();
-        playPauseButton.textContent = "Reproduzir";
-    }
-
-    if (video.muted) {
-        video.muted = false;
+        video.pause()
+        playPauseButton.textContent = "Reproduzir"
     }
 }
 
-const video = document.getElementById('vsl-video')
-video.addEventListener('ended', function() {
-    const videoBuyButton = document.getElementById('video-buy-button');
-    const headerBuyButton = document.getElementById('header-buy-button');
-    const infoBuyButton = document.getElementById('info-buy-button');
-    const footerBuyButton = document.getElementById('footer-buy-button');
-    
-    videoBuyButton.style.display = 'inline-block';
-    headerBuyButton.style.display = 'inline-block';
-    infoBuyButton.style.display = 'inline-block';
-    footerBuyButton.style.display = 'inline-block';
+document.addEventListener('DOMContentLoaded', function () {
+    const video = document.getElementById('vsl-video')
+    video.play().catch(error => {
+        console.error('Erro ao reproduzir o video.', error)
+    })
+
+    video.addEventListener('ended', function () {
+        const videoBuyButton = document.getElementById('video-buy-button')
+        videoBuyButton.style.display = 'inline-block'
+    })
 })
