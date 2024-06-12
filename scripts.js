ae0656e3ee26ed73f5cc2ed46c4184fea1ad2b6f
Lucas Santos <115504039+LucasSantos14@users.noreply.github.com>
2024-06-12 14:34:25 -0300

    Project Finalized

--- scripts.js
+++ scripts.js
@@ -1,25 +1,3 @@
 function buyNow() {
     window.location.href = "https://pay.kiwify.com.br/OXZo2kX"
 }
-
-function togglePlayPause() {
-    const video = document.getElementById('vsl-video')
-    const playPauseButton = document.getElementById('play-pause-button')
-
-    if (video.paused) {
-        video.play()
-        playPauseButton.textContent = "Pausar"
-    } else {
-        video.pause()
-        playPauseButton.textContent = "Reproduzir"
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    const video = document.getElementById('vsl-video')
-
-    video.addEventListener('ended', function () {
-        const videoBuyButton = document.getElementById('video-buy-button')
-        videoBuyButton.style.display = 'inline-block'
-    })
-})
